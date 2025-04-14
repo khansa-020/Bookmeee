be31eefdff0093d69ab528bff38475b56b1d1f4b
khansa abdullah <khansaaraein@gmail.com>
2025-04-14 16:11:07 +0500

    MongoDb Connected successfully

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();         // Load .env
+connectDB();             // Connect DB
+
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Backend is working!');
+  res.send('API is running...');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
