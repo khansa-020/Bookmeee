76d265b3ef3791300b0d31cec8326d1155184169
khansa abdullah <khansaaraein@gmail.com>
2025-04-18 11:50:17 +0500

    manage routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+// import QA from "./Pages/QA";
+// import TC from "./Pages/TC";
+import ContactUs from "./Pages/ContactUs";
 
 function App() {
   return (
-    <div className="App">
-
-guide me AllahS
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/QA" element={<QA/>} />
+        <Route path="/TC" element={<TC/>} /> */}
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+    </Router>
   );
 }
 
